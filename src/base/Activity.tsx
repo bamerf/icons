658d2be389d3143424cb8cb94d8d809471057cf8
Bamdad Erfanian <47050979+bamerf@users.noreply.github.com>
2022-11-11 12:19:59 +0300

    perf(icon): add license & test ref

--- src/base/Activity.tsx
+++ src/base/Activity.tsx
@@ -1,27 +1,26 @@
-import React from 'react';
-import { forwardRef } from 'react';
+import type { FC } from 'react';
 import type { CommonProps } from '../types';
 
-const Activity = forwardRef<SVGSVGElement, CommonProps>(
-  ({ color = 'currentColor', size = 24 }, ref) => 
-     (
-      <svg
-        ref={ref}
-        width={size}
-        height={size}
-        viewBox="0 0 24 24"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M22 12H18L15 21L9 3L6 12H2"
-          stroke={color}
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </svg>
-    )
+const Activity: FC<CommonProps> = (
+  { color = 'currentColor', size = 24 },
+  ref
+) => (
+  <svg
+    ref={ref}
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M22 12H18L15 21L9 3L6 12H2"
+      stroke={color}
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
 );
 
 export default Activity;
