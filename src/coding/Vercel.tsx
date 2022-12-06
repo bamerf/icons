4ffda5a4601452e6b54c4cc9c3535226c7bf74fe
Bamdad Erfanian <47050979+bamerf@users.noreply.github.com>
2022-12-06 20:30:09 +0300

    perf(codingIcon): add all coding icons

---
+++ src/coding/Vercel.tsx
@@ -0,0 +1,22 @@
+import { forwardRef } from 'react';
+import type { CommonProps } from '../types';
+
+const Vercel = forwardRef<SVGSVGElement, CommonProps>(({ size = 24 }, ref) => (
+  <svg
+    width={size}
+    height={size}
+    viewBox="0 0 32 32"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle cx="16" cy="16" r="14" fill="#171717" />
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M16 9L23 20.6667H9L16 9Z"
+      fill="#F5F5F5"
+    />
+  </svg>
+));
+
+export default Vercel;
